320db95b405d72f5bb82e6c8fef2c62cb9aa92c3
Hoang Kiem <kiemhd@outlook.com>
2024-02-19 00:42:29 +0700

    chore: re-format code

--- src/BladeTemplate.php
+++ src/BladeTemplate.php
@@ -14,72 +14,73 @@ use Illuminate\View\FileViewFinder;
 
 class BladeTemplate
 {
-	/**
-	 * @var Dispatcher
-	 */
-	private $dispatcher;
-	/**
-	 * @var Filesystem
-	 */
-	private $files;
-	/**
-	 * @var EngineResolver
-	 */
-	private $engineResolver;
-	/**
-	 * @var FileViewFinder
-	 */
-	private $finder;
-	/**
-	 * @var Factory
-	 */
-	private $factory;
-	/**
-	 * @var BladeCompiler
-	 */
-	private $compiler;
+    /**
+     * @var Dispatcher
+     */
+    private $dispatcher;
+    /**
+     * @var Filesystem
+     */
+    private $files;
+    /**
+     * @var EngineResolver
+     */
+    private $engineResolver;
+    /**
+     * @var FileViewFinder
+     */
+    private $finder;
+    /**
+     * @var Factory
+     */
+    private $factory;
+    /**
+     * @var BladeCompiler
+     */
+    private $compiler;
 
-	private $compileDir;
+    private $compileDir;
 
-	private $url;
+    private $url;
 
-	public function __construct(array $settings)
-	{
-		$this->dispatcher = new Dispatcher(new Container);
-		$this->files = new Filesystem;
-		$this->engineResolver = new EngineResolver;
+    public function __construct(array $settings)
+    {
+        $this->dispatcher = new Dispatcher(new Container);
+        $this->files = new Filesystem;
+        $this->engineResolver = new EngineResolver;
 
-		$this->finder = new FileViewFinder($this->files, [$settings['view']]);
-		$this->compiler = new BladeCompiler($this->files,  $settings['cache']);
+        $this->finder = new FileViewFinder($this->files, [$settings['view']]);
+        $this->compiler = new BladeCompiler($this->files,  $settings['cache']);
 
-		$this->engineResolver->register('blade', function () {
-			return new CompilerEngine($this->compiler);
-		});
+        $this->engineResolver->register(
+            'blade', function () {
+                return new CompilerEngine($this->compiler);
+            }
+        );
 
-		$this->compileDir = $settings['compileDir'];
-		$this->url = $settings['url'];
-		$this->factory = new Factory($this->engineResolver, $this->finder, $this->dispatcher);
-	}
+        $this->compileDir = $settings['compileDir'];
+        $this->url = $settings['url'];
+        $this->factory = new Factory($this->engineResolver, $this->finder, $this->dispatcher);
+    }
 
-	public function render($view, $data = [])
-	{
-		return $this->factory->make($view, $data)->render();
-	}
+    public function render($view, $data = [])
+    {
+        return $this->factory->make($view, $data)->render();
+    }
 
-	public function compiles()
-	{
-		$client = new Client();
-		$page = isset($_REQUEST['f']) ? $_REQUEST['f'] : 'index';
-		$fullUrl = $this->url.'/?f=' . $page;
-		if (!is_dir($this->compileDir))
-		{
-			mkdir($this->compileDir, 0777, true);
-		}
+    public function compiles()
+    {
+        $client = new Client();
+        $page = isset($_REQUEST['f']) ? $_REQUEST['f'] : 'index';
+        $fullUrl = $this->url.'/?f=' . $page;
+        if (!is_dir($this->compileDir)) {
+            mkdir($this->compileDir, 0777, true);
+        }
 
-		$response = $client->request('GET', $fullUrl);
-		$html = $response->getBody();
+        $response = $client->request('GET', $fullUrl);
+        $html = $response->getBody();
         $html = str_replace('="resources/assets/', '="assets/', $html);
-		$this->files->put($this->compileDir.'/'.$page . '.html', $html);
-		return true;
-	}
+        $this->files->put($this->compileDir.'/'.$page . '.html', $html);
+        return true;
+    }
 }
