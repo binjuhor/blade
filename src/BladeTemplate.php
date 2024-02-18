e9c7bb29425762c5b5e0aeb6c76159aa53699171
Hoang Kiem <kiemhd@outlook.com>
2024-02-18 23:47:42 +0700

    feat: add compiles functions

--- src/BladeTemplate.php
+++ src/BladeTemplate.php
@@ -2,6 +2,7 @@
 
 namespace Binjuhor\Blade;
 
+use GuzzleHttp\Client;
 use Illuminate\Container\Container;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Filesystem\Filesystem;
@@ -38,24 +39,46 @@ class BladeTemplate
 	 */
 	private $compiler;
 
-	public function __construct($view, $cache)
+	private $compileDir;
+
+	private $url;
+
+	public function __construct(array $settings)
 	{
 		$this->dispatcher = new Dispatcher(new Container);
 		$this->files = new Filesystem;
 		$this->engineResolver = new EngineResolver;
 
-		$this->finder = new FileViewFinder($this->files, [$view]);
-		$this->compiler = new BladeCompiler($this->files,  $cache);
+		$this->finder = new FileViewFinder($this->files, [$settings['view']]);
+		$this->compiler = new BladeCompiler($this->files,  $settings['cache']);
 
 		$this->engineResolver->register('blade', function () {
 			return new CompilerEngine($this->compiler);
 		});
 
+		$this->compileDir = $settings['compileDir'];
+		$this->url = $settings['url'];
 		$this->factory = new Factory($this->engineResolver, $this->finder, $this->dispatcher);
 	}
 
 	public function render($view, $data = [])
 	{
 		return $this->factory->make($view, $data)->render();
+	}
+
+	public function compiles()
+	{
+		$client = new Client();
+		$page = isset($_REQUEST['f']) ? $_REQUEST['f'] : 'index';
+		$fullUrl = $this->url.'/?f=' . $page;
+		if (!is_dir($this->compileDir))
+		{
+			mkdir($this->compileDir, 0777, true);
+		}
+
+		$response = $client->request('GET', $fullUrl);
+		$html = $response->getBody();
+		$this->files->put($this->compileDir.'/'.$page . '.html', $html);
+		return true;
 	}
 }
